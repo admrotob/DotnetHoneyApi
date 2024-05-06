cd6a79ba647303b1f9203e61402b8861cfd68379
Andres Martinez <andres.martinezd@outlook.com>
2024-05-06 01:05:30 -0400

    Cleaning up middleware

--- Authentication/ApiKeyAuthMiddleware.cs
+++ Authentication/ApiKeyAuthMiddleware.cs
@@ -3,7 +3,6 @@
     public class ApiKeyAuthMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly ILogger<ApiKeyAuthMiddleware> _logger;
 
         public ApiKeyAuthMiddleware(RequestDelegate next)
         {
